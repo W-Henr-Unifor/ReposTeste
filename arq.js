0d824757a5afc22ecd1d06572a04a38e3d1eb8ff
Lucas Oliveira <franlucasoliveira@edu.unifor.br>
2025-03-24 20:09:16 -0300

    Adicionando arquivo arq.js

--- arq.js
+++ arq.js
@@ -1,3 +1,22 @@
-document.getElementById('botao').onclick = function() {
-    document.getElementById('mensagem').innerHTML = 'Você clicou no botão!';
-};
+function soma(a,b){
+    return a + b;
+}
+
+function subtraçao(a,b){
+    return a - b;
+}
+
+function multiplicaçao(a,b){
+    return a * b;
+}
+
+function divisao(a,b){
+    return a / b;
+}
+
+console.log(soma(3,8));
+
+let temp = multiplicacao(2,3);
+let temp1 = divisao(8,2);
+
+console.log(subtraçao(10,5));
